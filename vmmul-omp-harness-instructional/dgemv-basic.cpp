a14f96e6b1266416425221f2ac8ceb4de8b2ea05
Gene Orias <57804515+cheeto2003@users.noreply.github.com>
2025-11-06 15:40:12 -0800

    Add files via upload

---
+++ vmmul-omp-harness-instructional/dgemv-basic.cpp
@@ -0,0 +1,19 @@
+const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
+
+/*
+ * This routine performs a dgemv operation
+ * Y :=  A * X + Y
+ * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
+ * On exit, A and X maintain their input values.
+ */
+void my_dgemv(int n, double* A, double* x, double* y) {
+   // insert your code here: implementation of basic matrix multiply
+   for (int i = 0; i < n; ++i) {
+      double sum = 0.0;
+      const double* Ai = A + static_cast<long long>(i) * n;
+      for (int j = 0; j < n; ++j) {
+         sum += Ai[j] * x[j];
+      }
+      y[i] += sum;
+   }
+}
