ae4998fec596473ca975da0a0022a368fb79de6d
Gene Orias <gene_o@x1004c2s6b0n0h0.chn.perlmutter.nersc.gov>
2025-11-11 16:59:18 -0800

    perlmutter run times added

---
+++ build/CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o: \
+ /global/homes/g/gene_o/csc-656-proj-3/dgemv-basic.cpp \
+ /usr/include/stdc-predef.h
---
+++ build/CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o: \
+ /global/homes/g/gene_o/csc-656-proj-3/dgemv-vectorized.cpp \
+ /usr/include/stdc-predef.h
